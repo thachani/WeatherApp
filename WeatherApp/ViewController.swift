6de65e0f0df2fe898f75f91cc095e9a138676d63
tarek hachani <tarek.hachani.ext@francetv.fr>
2020-08-16 23:21:06 +0200

    Initial project setup

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Tarek HACHANI on 16/08/2020.
+//  Copyright © 2020 Tarek-HACHANI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
